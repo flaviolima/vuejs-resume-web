b9aa606eaea4adf0136847d6c7ebd54f3051b71d
flaviolima <contato.flaviolima@gmail.com>
2022-03-24 14:14:09 -0300

    base project

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="height: 100px;" class="text-center items flex items-end justify-center mb-11">
+    <p>&#xA9 2022 - made with vueJS in SP &#9829;</p>
+  </div>
+</template>
+
---
+++ components/Skills.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div
+  class="p-12 text-center relative overflow-hidden bg-no-repeat bg-cover rounded-lg"
+  style="height: 100%; background-color: #F7FAFC"
+>
+
+    <div class="grid grid-cols-3 gap-4 flex items-center text-4xl">
+
+        <div class="mb-4">
+            <p>+9 years of experience</p>
+        </div>
+        <div class="mb-4">
+           <p>focused on results</p>
+        </div>
+         <div class="mb-4">
+            <p>web development expertise</p>
+        </div>
+
+    </div>
+   </div>
+
+</template>
