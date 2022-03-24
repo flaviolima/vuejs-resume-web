1ade7f035c9a61e59e6ffeef13626dc7635cd79a
flaviolima <contato.flaviolima@gmail.com>
2022-03-24 14:26:16 -0300

    base project

---
+++ .nuxt/components/index.js
@@ -0,0 +1,36 @@
+export { default as About } from '../../components/About.vue'
+export { default as Banner } from '../../components/Banner.vue'
+export { default as Footer } from '../../components/Footer.vue'
+export { default as Menu } from '../../components/Menu.vue'
+export { default as Skills } from '../../components/Skills.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
